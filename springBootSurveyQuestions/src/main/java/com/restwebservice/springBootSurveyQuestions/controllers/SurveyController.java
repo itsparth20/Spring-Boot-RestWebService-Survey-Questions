c5e6e6e8348b99e908182f65f0c2915ff80d14e2
Parth <itsparth20@gmail.com>
2017-09-13 01:02:04 -0700

    Added Survey Controller and Change Spring boot version.

---
+++ springBootSurveyQuestions/src/main/java/com/restwebservice/springBootSurveyQuestions/controllers/SurveyController.java
@@ -0,0 +1,23 @@
+package com.restwebservice.springBootSurveyQuestions.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restwebservice.springBootSurveyQuestions.model.Question;
+import com.restwebservice.springBootSurveyQuestions.services.SurveyService;
+
+@RestController
+public class SurveyController {
+	
+	@Autowired
+	SurveyService surveyService;
+	
+	@GetMapping("/surveys/{surveyId}/questions")
+	public List<Question> getQuestionsForSurvey(@PathVariable String surveyId){
+		return surveyService.retrieveQuestions(surveyId);
+	}
+}
