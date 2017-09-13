13bf13cc51e73055d99d44f11e5e4c5eefc72a25
Parth <itsparth20@gmail.com>
2017-09-13 00:11:19 -0700

    Init

---
+++ springBootSurveyQuestions/src/main/java/com/restwebservice/springBootSurveyQuestions/SpringBootSurveyQuestionsApplication.java
@@ -0,0 +1,12 @@
+package com.restwebservice.springBootSurveyQuestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSurveyQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSurveyQuestionsApplication.class, args);
+	}
+}
