0d165167db20f5b463d4a8a78ffce5adf03768e9
Parth <itsparth20@gmail.com>
2017-09-28 20:51:55 -0700

    Spring Data Rest

--- springBootSurveyQuestions/src/main/java/com/restwebservice/springBootSurveyQuestions/jpa/UserRepository.java
+++ springBootSurveyQuestions/src/main/java/com/restwebservice/springBootSurveyQuestions/jpa/UserRepository.java
@@ -2,8 +2,12 @@ package com.restwebservice.springBootSurveyQuestions.jpa;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long>{
-	List<User> findByRole(String description);
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="users", collectionResourceRel="users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long>{
+	List<User> findByRole(@Param("role") String description);
 }
